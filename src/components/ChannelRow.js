2f8d7a062d2cf81560a0af92badd2a2923baa9b4
Joseph Carson <josephc89@fullsail.edu>
2022-09-12 17:43:54 -0400

    update_5

--- src/components/ChannelRow.js
+++ src/components/ChannelRow.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { Avatar } from "@material-ui/core";
 import VerifiedIcon from "@material-ui/icons/CheckCircleOutlineSharp";
 import "../css/ChannelRow.css";
--- src/components/Search/SearchPage.js
+++ src/components/Search/SearchPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import ChannelRow from "../ChannelRow";
 import VideoRow from "../VideoRow";
 import TuneSharpIcon from "@material-ui/icons/TuneSharp";
