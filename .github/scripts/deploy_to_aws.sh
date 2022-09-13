23222c3b25b9155ca8d423617e762731443ebc26
Joseph Carson <josephc89@fullsail.edu>
2022-09-12 20:51:00 -0400

    update 34

--- .github/scripts/deploy_to_aws.sh
+++ .github/scripts/deploy_to_aws.sh
@@ -1,12 +1,13 @@
 echo 'Starting Deployment...'
-ssh -o StrictHostKeyChecking=no -i /home/${USERNAME}/.ssh/github_action ${USERNAME}@${HOST}
-            # Now we have got the access of EC2 and we will start the deploy .
-          ` 
-            ls
-            # git checkout master &&
-            # git fetch --all &&
-            # git reset --hard origin/master &&
-            # git pull origin master &&
-            # cp build/. -r /var/www/html
-          `
+
+`
+rm -r build
+git checkout master &&
+git fetch --all &&
+git reset --hard origin/master &&
+git pull origin master &&
+cp build/. -r /var/www/html
+rm -r build
+`
+
 echo 'Deployment completed successfully'
