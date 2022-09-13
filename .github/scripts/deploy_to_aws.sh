5f922826bea9c937c1a444f2a007c225b6f0bc30
Joseph Carson <josephc89@fullsail.edu>
2022-09-12 20:03:26 -0400

    update 30

--- .github/scripts/deploy_to_aws.sh
+++ .github/scripts/deploy_to_aws.sh
@@ -1,5 +1,6 @@
 echo 'Starting Deployment...'
-ssh -o StrictHostKeyChecking=no -i /home/${USERNAME}/.ssh/github_action ${USERNAME}@${HOST_DNS}
+ssh -o StrictHostKeyChecking=no -i /home/${USERNAME}/.ssh/github_action ${USERNAME}@${HOST}
+echo `Host: ${HOST} - HOST: $HOST`
             # Now we have got the access of EC2 and we will start the deploy .
           ` 
             cd /home/${USERNAME}/youtubeClone &&
@@ -7,6 +8,6 @@ ssh -o StrictHostKeyChecking=no -i /home/${USERNAME}/.ssh/github_action ${USERNA
             git fetch --all &&
             git reset --hard origin/master &&
             git pull origin master &&
-            cp build/. var/www/html
+            cp build/. -r /var/www/html
           `
 echo 'Deployment completed successfully'
